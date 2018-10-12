c78ab5bbcbf7f952a36d22415b3163697df45967
bahrus <anderson.bruce.b@gmail.com>
2018-10-12 07:20:34 -0400

    ...

--- purr-sist.js
+++ purr-sist.js
@@ -2,11 +2,12 @@ import { XtallatX } from 'xtal-latx/xtal-latx.js';
 import { define } from 'xtal-latx/define.js';
 const store_id = 'store-id';
 const save_service_url = 'save-service-url';
-const new_val = 'new_val';
+//const new_val = 'new_val';
 export class PurrSist extends XtallatX(HTMLElement) {
     constructor() {
         super(...arguments);
         this._saveServiceUrl = 'https://api.myjson.com/bins';
+        this._initInProgress = false;
     }
     static get is() { return 'purr-sist'; }
     static get observedAttributes() {
@@ -40,6 +41,12 @@ export class PurrSist extends XtallatX(HTMLElement) {
         return this._newVal;
     }
     set newVal(val) {
+        if (!this._storeId) {
+            if (!this._pendingNewVals)
+                this._pendingNewVals = [];
+            this._pendingNewVals.push(val);
+            return;
+        }
         this._newVal = val;
         fetch(this._saveServiceUrl + '/' + this._storeId, {
             headers: {
@@ -55,25 +62,34 @@ export class PurrSist extends XtallatX(HTMLElement) {
         });
     }
     connectedCallback() {
-        this._upgradeProperties(['disabled', store_id, save_service_url]);
+        this._upgradeProperties([store_id, save_service_url, 'disabled']);
         this._conn = true;
         this.onPropsChange();
     }
     onPropsChange() {
-        if (!this._conn || !this._saveServiceUrl)
+        if (!this._conn || !this._saveServiceUrl || this._disabled)
             return;
         if (!this._storeId) {
             //create new object
+            if (this._initInProgress)
+                return;
             fetch(this._saveServiceUrl, {
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },
                 method: 'POST',
-                body: JSON.stringify({ a: 'b' }),
+                body: JSON.stringify({ origin: window.location }),
             }).then(resp => {
                 resp.json().then(json => {
-                    this.innerText = `Please set store-id = ${json.uri.split('/').pop()}`;
+                    this._initInProgress = false;
+                    this.storeId = json.uri.split('/').pop();
+                    if (this._pendingNewVals) {
+                        this._pendingNewVals.forEach(kvp => {
+                            this.newVal = kvp;
+                        });
+                        delete this._pendingNewVals;
+                    }
                 });
             });
         }
--- purr-sist.ts
+++ purr-sist.ts
@@ -1,21 +1,21 @@
-import {XtallatX} from 'xtal-latx/xtal-latx.js';
-import {define} from 'xtal-latx/define.js';
-interface KVP{
+import { XtallatX } from 'xtal-latx/xtal-latx.js';
+import { define } from 'xtal-latx/define.js';
+interface KVP {
     key: string;
     value: any;
 }
 const store_id = 'store-id';
 const save_service_url = 'save-service-url';
-const new_val = 'new_val';
-export class PurrSist extends XtallatX(HTMLElement){
-    static get is(){return 'purr-sist';}
-    
-    static get observedAttributes(){
+//const new_val = 'new_val';
+export class PurrSist extends XtallatX(HTMLElement) {
+    static get is() { return 'purr-sist'; }
+
+    static get observedAttributes() {
         return super.observedAttributes.concat([store_id]);
     }
-    attributeChangedCallback(n: string, ov: string, nv: string){
+    attributeChangedCallback(n: string, ov: string, nv: string) {
         super.attributeChangedCallback(n, ov, nv);
-        switch(n){
+        switch (n) {
             case store_id:
                 this._storeId = nv;
                 break;
@@ -26,35 +26,41 @@ export class PurrSist extends XtallatX(HTMLElement){
         this.onPropsChange()
     }
     _storeId!: string;
-    get storeId(){
+    get storeId() {
         return this._storeId;
     }
-    set storeId(val){
+    set storeId(val) {
         this.attr(store_id, val);
     }
     _saveServiceUrl: string = 'https://api.myjson.com/bins';
-    get saveServiceUrl(){
+    get saveServiceUrl() {
         return this._saveServiceUrl;
     }
-    set saveServiceUrl(val){
+    set saveServiceUrl(val) {
         this.attr(save_service_url, val);
     }
+    _pendingNewVals!: KVP[];
     _newVal: any;
-    get newVal(){
+    get newVal() {
         return this._newVal;
     }
-    set newVal(val: KVP){
+    set newVal(val: KVP) {
+        if(!this._storeId){
+            if(!this._pendingNewVals) this._pendingNewVals = [];
+            this._pendingNewVals.push(val);
+            return;
+        }
         this._newVal = val;
         fetch(this._saveServiceUrl + '/' + this._storeId, {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
-              },
-            method: 'PUT', 
-            body: JSON.stringify(val), 
+            },
+            method: 'PUT',
+            body: JSON.stringify(val),
 
-        }).then(resp =>{
-            this.de('newVal',{
+        }).then(resp => {
+            this.de('newVal', {
                 value: val,
             })
         })
@@ -62,32 +68,42 @@ export class PurrSist extends XtallatX(HTMLElement){
 
     _conn!: boolean;
 
-    connectedCallback(){
-        this._upgradeProperties(['disabled', store_id, save_service_url]);
+    connectedCallback() {
+        this._upgradeProperties([store_id, save_service_url, 'disabled']);
         this._conn = true;
         this.onPropsChange();
     }
     value: any;
-    onPropsChange(){
-        if(!this._conn || !this._saveServiceUrl) return;
-        if(!this._storeId){
+    _initInProgress = false;
+    onPropsChange() {
+        if (!this._conn || !this._saveServiceUrl || this._disabled) return;
+        if (!this._storeId) {
             //create new object
+            if (this._initInProgress) return;
             fetch(this._saveServiceUrl, {
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
-                  },
-                method: 'POST', 
-                body: JSON.stringify({a: 'b'}),
+                },
+                method: 'POST',
+                body: JSON.stringify({origin: window.location}),
 
-            }).then(resp =>{
-                resp.json().then(json =>{
-                    this.innerText =`Please set store-id = ${json.uri.split('/').pop()}`;
+            }).then(resp => {
+                resp.json().then(json => {
+                    this._initInProgress = false;
+                    this.storeId = json.uri.split('/').pop();
+                    if(this._pendingNewVals){
+                        this._pendingNewVals.forEach(kvp =>{
+                            this.newVal = kvp;
+                        })
+                        delete this._pendingNewVals;
+                    }
                 })
+
             })
-        }else{
-            fetch(this._saveServiceUrl + '/' + this._storeId).then(resp =>{
-                resp.json().then(json =>{
+        } else {
+            fetch(this._saveServiceUrl + '/' + this._storeId).then(resp => {
+                resp.json().then(json => {
                     this.value = json;
                     this.de('value', {
                         value: json
