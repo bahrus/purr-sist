614d85bdf423532a93465c47c7fc7eb29cdb4afb
bahrus <anderson.bruce.b@gmail.com>
2018-12-04 20:26:08 -0500

    ...

--- createIIFE.js
+++ createIIFE.js
@@ -1,7 +1,7 @@
 //@ts-check
 const jiife = require('jiife');
 const xl = 'node_modules/xtal-latx/';
-jiife.processFiles([xl + 'xtal-latx.js', xl + 'define.js', xl + 'base-link-id.js', 'purr-sist.js'], 'purr-sist.iife.js');
+jiife.processFiles([xl + 'xtal-latx.js', xl + 'define.js', xl + 'base-link-id.js', 'purr-sist.js', 'purr-sist-myjson.js'], 'dist/purr-sist-myjson.iife.js');
 
 
 
--- dist/purr-sist-myjson.iife.js
+++ dist/purr-sist-myjson.iife.js
@@ -71,7 +71,7 @@ function XtallatX(superClass) {
          * @param detail Information to be passed with the event
          * @param asIs If true, don't append event name with '-changed'
          */
-        de(name, detail, asIs) {
+        de(name, detail, asIs = false) {
             const eventName = name + (asIs ? '' : '-changed');
             const newEvent = new CustomEvent(eventName, {
                 detail: detail,
@@ -318,5 +318,100 @@ class PurrSist extends BaseLinkId(XtallatX(HTMLElement)) {
     }
 }
 // define(PurrSist);
+const save_service_url = 'save-service-url';
+class PurrSistMyJson extends PurrSist {
+    static get is() { return 'purr-sist-myjson'; }
+    static get observedAttributes() {
+        return super.observedAttributes.concat([save_service_url]);
+    }
+    attributeChangedCallback(n, ov, nv) {
+        switch (n) {
+            case save_service_url:
+                this._saveServiceUrl = nv;
+                break;
+        }
+        super.attributeChangedCallback(n, ov, nv);
+    }
+    createNew(master) {
+        if (this._initInProgress)
+            return;
+        const val = {};
+        fetch(this._saveServiceUrl, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(val),
+        }).then(resp => {
+            resp.json().then(json => {
+                this._initInProgress = false;
+                this.storeId = json.uri.split('/').pop();
+                if (this._pendingNewVals) {
+                    this._pendingNewVals.forEach(kvp => {
+                        this.newVal = kvp;
+                    });
+                    delete this._pendingNewVals;
+                }
+                if (master !== null)
+                    master.newVal = {
+                        [this._guid]: this._storeId,
+                    };
+            });
+        });
+        this.value = val;
+    }
+    get saveServiceUrl() {
+        return this._saveServiceUrl;
+    }
+    set saveServiceUrl(val) {
+        this.attr(save_service_url, val);
+    }
+    saveNewVal(value) {
+        fetch(this._saveServiceUrl + '/' + this._storeId, {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: 'PUT',
+            body: JSON.stringify(value),
+        }).then(resp => {
+            // this.de('newVal', {
+            //     value: val,
+            // })
+            this.value = value;
+        });
+    }
+    connectedCallback() {
+        this._upgradeProperties(['saveServiceUrl']);
+        if (!this._saveServiceUrl) {
+            if (this._baseLinkId) {
+                this._saveServiceUrl = this.getFullURL('');
+            }
+            else {
+                this._saveServiceUrl = 'https://api.myjson.com/bins';
+            }
+        }
+        super.connectedCallback();
+    }
+    onPropsChange(n) {
+        if (!this._saveServiceUrl)
+            return;
+        super.onPropsChange();
+    }
+    getStore() {
+        if (this._fip)
+            return;
+        this._fip = true;
+        fetch(this._saveServiceUrl + '/' + this._storeId).then(resp => {
+            resp.json().then(json => {
+                json.__purrSistInit = true;
+                this.value = json;
+                this._fip = false;
+            });
+        });
+    }
+}
+define(PurrSistMyJson);
     })();  
         
